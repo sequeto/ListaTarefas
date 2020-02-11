a9e380fc0cb301e46a89c24e879f3d957a180aae
sequeto <joaopedrosequeto@gmail.com>
2020-02-11 15:57:25 -0300

    Adiciona função de inclusão e exclusão de itens.

--- lista.js
+++ lista.js
@@ -1,20 +1,45 @@
 var listElement = document.querySelector("#aplicacao ul");
-var inputElement = document.querySelector("#aplicacao input");
-var buttonElement = document.querySelector("#aplicacao button");
+var inputElement = document.querySelector("#entrada");
+var buttonElement = document.querySelector("#botao");
 
 var tarefas = [
-    
-]
+];
 
 function renderLista(){
+
+    listElement.innerHTML = '';
+
     for(item of tarefas)
     {
-        var itemElement = document.createElement('li');
+        var itemElement = document.createElement('li'); // Cria elemento da lista
         var itemTexto = document.createTextNode(item);
+        var linkElement = document.createElement('a'); // Cria link para exclusão
+        var linkText = document.createTextNode(' Excluir');
+        var posicao = tarefas.indexOf(item); // Informa posição do item no vetor
+
+        linkElement.setAttribute('href', '#');
+        linkElement.setAttribute('onclick', 'deletaTarefa('+ posicao +')'); // Associa a função de exclusão ao link
+        linkElement.appendChild(linkText);
         
         itemElement.appendChild(itemTexto);
-        listElement.appendChild(itemElement);
+        itemElement.appendChild(linkElement);
+
+        listElement.appendChild(itemElement); // Adiciona o item na lista
     }
 }
 
 renderLista();
+
+function adicionaTarefa(){
+    var entrada = inputElement.value;
+    tarefas.push(entrada); // Adiciona no vetor de tarefas o item informado
+    inputElement.value = '';
+    renderLista();
+}
+
+buttonElement.onclick = adicionaTarefa;
+
+function deletaTarefa(posicao){
+    tarefas.splice(posicao, 1); // Remove no vetor de tarefas na posição indicada
+    renderLista();
+}
